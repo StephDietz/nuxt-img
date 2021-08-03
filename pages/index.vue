3709851dc3c696780b628bc6f831ebc5a3022f16
Stephani Dietz <steph@waya.ai>
2021-08-02 23:10:30 -0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="h-screen w-screen bg-black p-8 flex items-center flex-col">
+    <h1 class="text-white text-center mb-4 font-bold">Nuxt Image</h1>
+    <nuxt-img
+      src="/8.png"
+      alt="Image"
+      sizes="xl:100vw lg:100vw md:100vw sm:100vw xs:100vw"/>
+  </div>
+</template>
